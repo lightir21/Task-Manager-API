31d2f823eb61f8a1ee98c05bee42993c76482e61
meir <lightir21@gmail.com>
2022-10-21 23:31:31 +0300

    created controllers and routes folders - and setup first api route
    -creating in the root directory controllers and routes folders
    -in routes created a taks.js file
    -required express & created router variable using express.Router()
    -setted first route router('/').get((req,res)=>{
    	res.send('all items')
    })
    didn't set the controller yet so imbaded the controller function inline
    -and exported it with module.exports = router
    
    -used a middle wear that alows me to access the body in the request object : app.use(express.json())
    -required the tasks router as tasks
    -accessed the route from out router : app.use('/api/v1/tasks', tasks)

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routers/tasks");
+
+// middlewear
+
+app.use(express.json());
 
 // routes
 app.get("/hello", (req, res) => {
   res.send("Task manager app");
 });
+
+app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
