91bf54fab609c48e5cea8518825e06caa096c107
meir <lightir21@gmail.com>
2022-10-23 02:22:56 +0300

    Install mongoose and Connect to DB
    -instal mongoose with 'npm i mongoose'
    
    connect.js:
    	-require mongoose and set to a variable called mongoose.
    	-connect to the db with mongoose and set the connection string as first argument using mongoose.connect(connectionString)
    	-it returns a promise so chain .then and .catch for catching errors
    
    app.js:
    	-in order to make the connection work we have to require the connect.js file inside app.js

--- db/connect.js
+++ db/connect.js
@@ -1,2 +1,16 @@
+const mongoose = require("mongoose");
+
 const connectionString =
-  "mongodb+srv://lightir21:<password>@node-express-projects.hwrpkou.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://lightir21:<password>@node-express-projects.hwrpkou.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority";
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("CONNECTED TO THE DB...");
+  })
+  .catch((err) => console.log(err));
