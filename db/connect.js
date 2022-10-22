684ed14e913eda9efd118ee72877dc673b8ca189
meir <lightir21@gmail.com>
2022-10-23 02:41:56 +0300

    Refactor connect.js and using .env file
    -instead of setting the connection to the db after the connection to the server we want to chage it so only if we got excess to the db we can use the server other wise it's not gonna work
    connect.js:
    	-create a function called connectDB which accept a url parameter and return the mongoose.connect(url) and export it
    
    -install dotend
    -create a .env file
    
    .env:
    	-set the connection string to a variable called MONGO_URI
    
    app.js:
    	-create a function called start make it async (because mongoose connect returns a promise)
    	-make a try catch block
    	-await the connect DB and insert the URL from the .env file process.env.MONGO_URI
    	-only after that listen to the server
    	-and in the catch block log the error

--- db/connect.js
+++ db/connect.js
@@ -1,16 +1,12 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://lightir21:<password>@node-express-projects.hwrpkou.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority";
-
-mongoose
-  .connect(connectionString, {
+const connectDB = (url) => {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("CONNECTED TO THE DB...");
-  })
-  .catch((err) => console.log(err));
+  });
+};
+
+module.exports = connectDB;
