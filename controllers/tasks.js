1b7af05d3b209daefdfdcff805ad9d64a32015c2
meir <lightir21@gmail.com>
2022-10-28 22:28:33 +0300

    Custom error
    -in order to cover 404 status codes we create a a custom error
    -create a folder called errors
    -create a file called custom-error.js
    
    custom-error.js:
    	// in order to throw a New error we gonna extend from the Error class
    	-create a class with the name of customAPIError and extends it from Error
    	-create a cunstructor function with a message and statusCode parameters
    	-inside the constructor create super functions and set it to message
    	-set  this.statusCode = statusCode
    	// the super will invoke the constructor of the parent class - so it will set the mesaage propery to what we pass in. and then we creat a new property on the object called statusCode and we set it to what we pass in
    
    	class CustomAPIError extends Error {
      constructor(message, statusCode) {
        super(message);
        this.statusCode = statusCode;
      }
    }
    
    	-creat a function called createCustomError with msg and statusCode parameters
    	-return a new CustomAPIerror instance and pass in the msg and status parameters
    	-export both createCustomError and customAPIError
    
    error-handler.js:
    	// in order to catch the New error from the customAPIError we have to check if it's an instance of it otherwise we will call a status of 500 like before
    	-require CustomAPIError
    	-at the top of the function before of the return create an if statement
    	-check if err is an instanceof CustomAPIErrror
    	-if it is return status of err.statusCode and send a json of msg: err.message
    
    tasks.js(controllers):
    	// in order to throw the error when there is no task on the controllers we have to pass in to next the createCustomError function with the arguments we want
    	-require createCustomError
    	-inside the if statement that checks if there is no task (field is empty) retun in the next function with createCustomError(`No task with id: ${taskID}`,404) - first parameter is the msg and second is the statusCode

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,6 +1,7 @@
 const { Mongoose } = require("mongoose");
 const Task = require("../models/Tasks");
 const asyncWrapper = require("../middleware/async");
+const { createCustomError } = require("../errors/custom-error");
 
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
@@ -16,12 +17,12 @@ const createTask = asyncWrapper(async (req, res) => {
   }
 });
 
-const getTask = asyncWrapper(async (req, res) => {
+const getTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID });
 
   if (!task) {
-    return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
   }
 
   res.status(200).json({ task });
@@ -33,7 +34,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
   const task = await Task.findOneAndDelete({ _id: taskID });
 
   if (!task) {
-    return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
   }
 
   res.status(200).json({ task });
@@ -48,7 +49,7 @@ const updateTask = asyncWrapper(async (req, res) => {
   });
 
   if (!task) {
-    return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
   }
 
   res.status(200).json({ task });
